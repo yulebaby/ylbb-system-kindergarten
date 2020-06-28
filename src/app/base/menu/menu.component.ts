03b1bdcb0eb52b4e7030b03dc923c5721a40f5dd
zhaopeng <874391057@qq.com>
2020-06-28 09:43:39 +0800

    ---------

--- src/app/base/menu/menu.component.ts
+++ src/app/base/menu/menu.component.ts
@@ -27,14 +27,34 @@ export class MenuComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    
+    this.store.select('userInfoState').subscribe(userInfo => {
+      const storeId = userInfo.kindergartenId;
+      let menuConfig = JSON.parse(JSON.stringify(this.menuConfig));
+      menuConfig.map(res => {
+        if(storeId == 1){
+          if(res.key == '/home/education'){
+            const children = res.children.filter(item=> item.key != '/home/education/plan' && item.key != '/home/education/curriculum' )
+            res.children = children;
+          }else if(res.key == '/home/setting'){
+            const children = res.children.filter(item=> item.key!= '/home/setting/list')
+            res.children = children;   
+          } 
+        }
+      });
+      this.menuConfig= menuConfig;
+    });
+
     this.store.select('routerState').subscribe(res => this.routerState = res);
 
     this.store.select('userInfoState').subscribe(res => {
       this.roleAllowPath = res.menuUrls;
     });
+    
   }
 
   routerLink(children: any[]) {
+    
     if (this.roleAllowPath.indexOf('**') > -1) {
       this.router.navigateByUrl(children[0].key);
     } else {
